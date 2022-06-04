925b1dcfd82d7731f20db4739bac6ab8dbd572e0
Atirut Wattanamongkol <atirut.wattanamongkol@gmail.com>
2022-06-04 09:23:17 +0700

    Improve boot reliability
    Also introduces a struct for disk controllers to make code easier to read

--- src/main.c
+++ src/main.c
@@ -3,28 +3,31 @@
 #include <string.h>
 #include "peekpoke.h"
 
+typedef struct {
+    volatile char uuid[2]; // First two byte of the UUID of the drive we want to do stuff with
+    volatile char sectorsize; // Size of a sector in units of 256 bytes
+    volatile char rw; // R/W port
+    volatile short sectorcount; // Number of sectors on the drive
+    volatile short rwsector; // Current sector we are reading or writing to
+} diskslot_t;
+
 void tryboot() {
-    char sectorsize = PEEK(0x262); // In units of 256 bytes
-    if (sectorsize == 0) {
-        return; // No drive mapped
+    diskslot_t* disk = (diskslot_t*)0x260;
+    if (disk->sectorsize == 0 || disk->sectorsize < 2) {
+        return;
     }
 
     // Set current R/W sector to 1
-    POKE(0x266, 1);
-    POKE(0x267, 0);
+    disk->rwsector = 1;
     char* ldptr = (char*)0x1000; // Address to load fist sector to
 
-    for (int i = 0; i < sectorsize; i++) {
-        for (int j = 0; j < 256; j++) {
-            *ldptr++ = PEEK(0x263);
-        }
+    for (int i = 0; i < 512; i++) {
+        ldptr[i] = disk->rw;
     }
 
-    // Verify for boot signature
-    if (*(short*)(ldptr - 2) == 0xaa55) {
+    // Verify for boot signature at 0x100 + 0x1FE
+    if (*(short*)0x11fe == 0xaa55) {
         putchar(7); // Beep
-
-        // (*(void(*)())0x1000)();
         asm volatile ("JMP $1000");
     }
 }
@@ -44,8 +47,9 @@ void finddrive() {
             // print("\r");
 
             // Map the drive to the first drive controller
-            POKE(0x260, comp.uuid[0]);
-            POKE(0x261, comp.uuid[1]);
+            diskslot_t* disk = (diskslot_t*)0x260;
+            disk->uuid[0] = comp.uuid[0];
+            disk->uuid[1] = comp.uuid[1];
             tryboot();
         }
 
@@ -59,9 +63,7 @@ int main() {
     putchar(6); // Cycle to next supported terminal size.
     print("Ocean BIOS Copyright 2022 Atirut Wattanamongkol\r\r");
 
-    for (int i = 0; i < 3; i++) {
-        finddrive(); // The code sometimes fail to validate the boot signature so retry a few times.
-    }
+    finddrive();
 
     print("No bootable drive found.\rInsert a drive with a valid boot sector and restart the system.\r");
     return 0;
