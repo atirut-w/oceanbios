b69c6376435d75bd63181e7717ec46c3c3616d7e
Atirut Wattanamongkol <atirut.wattanamongkol@gmail.com>
2022-06-15 16:23:17 +0700

    Clean up and rewrite some stuff

--- src/main.c
+++ src/main.c
@@ -2,6 +2,7 @@
 #include "component.h"
 #include <string.h>
 #include "peekpoke.h"
+#include <stdbool.h>
 
 typedef struct {
     volatile char uuid[2]; // First two byte of the UUID of the drive we want to do stuff with
@@ -11,60 +12,56 @@ typedef struct {
     volatile short rwsector; // Current sector we are reading or writing to
 } diskslot_t;
 
-void tryboot() {
-    diskslot_t* disk = (diskslot_t*)0x260;
-    if (disk->sectorsize == 0 || disk->sectorsize < 2) {
-        return;
+diskslot_t* disk = (diskslot_t*)0x260;
+
+// Load the boot sector of a disk into memory starting from 0x1000
+void load_bs() {
+    if (disk->sectorsize < 2) {
+        return; // Must be at least 512 bytes
     }
 
-    // Set current R/W sector to 1
-    disk->rwsector = 1;
-    char* ldptr = (char*)0x1000; // Address to load fist sector to
-
+    disk->rwsector = 1; // Sector starts at 1 in OpenComputers
     for (int i = 0; i < 512; i++) {
-        ldptr[i] = disk->rw;
-    }
-
-    // Verify for boot signature at 0x100 + 0x1FE
-    if (*(short*)0x11fe == 0xaa55) {
-        putchar(7); // Beep
-        asm volatile ("JMP $1000");
+        POKE(0x1000 + i, disk->rw);
     }
 }
 
-void finddrive() {
+// Look for disks with a valid boot signature
+bool find_disk() {
     component_t comp;
-
     complist_start();
     read_component(&comp);
 
     while (comp.name[0] != 0xff) {
+        // print(comp.name);
+        // print("\r");
         if (memcmp(comp.name, "drive", 6) == 0) {
-            // print("Checking drive ");
-            // for (int i = 0; i < 2; i++) {
-            //     printbyte(comp.uuid[i]);
-            // }
-            // print("\r");
-
-            // Map the drive to the first drive controller
-            diskslot_t* disk = (diskslot_t*)0x260;
             disk->uuid[0] = comp.uuid[0];
             disk->uuid[1] = comp.uuid[1];
-            tryboot();
+
+            load_bs();
+            if (*(short*)0x11fe == 0xaa55) {
+                return true;
+            }
         }
 
         next_component();
         read_component(&comp);
     }
+
+    return 0;
 }
 
 int main() {
-    putchar(0); // Initialize the terminal.
-    putchar(6); // Cycle to next supported terminal size.
+    putchar(0); // Initialize the terminal
+    putchar(6); // Cycle to next supported terminal size
     print("Ocean BIOS Copyright 2022 Atirut Wattanamongkol\r\r");
 
     for (int i = 0; i < 2; i++) {
-        finddrive(); // Try at least two times. First time sometimes doesn't work for some reason.
+        if (find_disk()) {
+            putchar(7); // Beep
+            asm ("jmp $1000"); // First try sometimes fail for some reason
+        }
     }
 
     print("No bootable drive found.\rInsert a drive with a valid boot sector and restart the system.\r");
