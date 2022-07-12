atirut-w/oceanbios

commit e00b1b7e9b32d7866562f04f078a49190a61c280

Fix wrong stack pointer value

src/main.c
@@ -64,7 +64,7 @@ int main()
         if (find_disk())
         {
             putchar(7); // Beep
-            asm("ldx #0");
+            asm("ldx #ff");
             asm("txs");       // Reset the stack pointer
             asm("jmp $1000"); // Jump to the boot sector
         }

commit 0b7084752be2803067524ddf4d3882c75b4105c5

Fix a bug introduced by e00b1b7

src/main.c
@@ -64,7 +64,7 @@ int main()
         if (find_disk())
         {
             putchar(7); // Beep
-            asm("ldx #ff");
+            asm("ldx #$ff");
             asm("txs");       // Reset the stack pointer
             asm("jmp $1000"); // Jump to the boot sector
         }