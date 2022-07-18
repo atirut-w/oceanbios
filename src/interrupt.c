60a16110812a7e3e92323c21642e1aa0fc43651c
Atirut Wattanamongkol <atirut.wattanamongkol@gmail.com>
2022-07-18 21:35:20 +0700

    Got `Y` register working

--- src/interrupt.c
+++ src/interrupt.c
@@ -1,11 +1,53 @@
 #include "terminal.h"
 
-void irq() __attribute__((interrupt))
+void irq() __attribute__((interrupt, no_isr))
 {
-    print("IRQ not implemented\r");
+    char a, x, y;
+    asm volatile(
+        "sta %0\n"
+        "stx %1\n"
+        "sty %2\n"
+        : "=m"(a), "=m"(x), "=m"(y));
+
+    print("TODO: Implement IRQ. A: $");
+    printbyte(a);
+    print(", X: $");
+    printbyte(x);
+    print(", Y: $");
+    printbyte(y);
+    print("\n");
+
+    asm(
+        "lda %0\n"
+        "ldx %1\n"
+        "ldy %2\n"
+        "rti\n" ::"m"(a),
+        "m"(x), "m"(y));
+    return;
 }
 
-void nmi() __attribute__((interrupt))
+void nmi() __attribute__((interrupt, no_isr))
 {
-    print("NMI not implemented\r");
+    char a, x, y;
+    asm volatile(
+        "sta %0\n"
+        "stx %1\n"
+        "sty %2\n"
+        : "=m"(a), "=m"(x), "=m"(y));
+
+    print("TODO: Implement NMI. A: $");
+    printbyte(a);
+    print(", X: $");
+    printbyte(x);
+    print(", Y: $");
+    printbyte(y);
+    print("\n");
+
+    asm(
+        "lda %0\n"
+        "ldx %1\n"
+        "ldy %2\n"
+        "rti\n" ::"m"(a),
+        "m"(x), "m"(y));
+    return;
 }
